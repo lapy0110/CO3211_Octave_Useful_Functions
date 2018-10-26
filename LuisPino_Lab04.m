3d029a99cf6763545b11adbc9c8f96b4e7da79d0
Luis Pino <lapy0110@gmail.com>
2018-10-26 18:59:42 -0400

    Prefinal

--- LuisPino_Lab04.m
+++ LuisPino_Lab04.m
@@ -41,7 +41,64 @@ STLU=sistemaLU(A,B);
 toc
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% EJERCICIO 2 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Y=-5+(5+5)*rand(250,250) %Creamos la matriz Y
-B=Y*transpose(Y)
-A=B+()
+Y=-5+(5+5)*rand(250,250); %Creamos la matriz Y
+B=Y*transpose(Y);
+A=B+(eps-abs(min(eig(B))))*I(250);
 
+e=[];
+for i=1:1:250
+    e(1,i)=(-1).^i;
+end
+
+%%% PREGUNTA A
+if (A==transpose(A))
+    Simetrica=1
+else
+    Simetrica=0
+end
+
+if (min(eig(A))>0)
+    DefPositiva=1
+else
+    DefPositiva=0
+end
+
+%%% PREGUNTA B
+% Para EPS=10.^-12
+A1=B+((10.^-12)-abs(min(eig(B))))*I(250);
+b1=A1*e;
+
+tic;
+LU1=sistemaLU(A1,b1);
+toc
+
+tic;
+C1=sistemaCholesky(A1,b1);
+toc
+
+% Para EPS=10.^-8
+A2=B+((10.^-8)-abs(min(eig(B))))*I(250);
+b2=A2*e;
+
+tic;
+LU2=sistemaLU(A2,b2);
+toc
+
+tic;
+C2=sistemaCholesky(A2,b2);
+toc
+% Para EPS=10.^-4
+A3=B+((10.^-4)-abs(min(eig(B))))*I(250);
+b3=A3*e;
+
+tic;
+LU3=sistemaLU(A3,b3);
+toc
+
+tic;
+C3=sistemaCholesky(A3,b3);
+toc
+
+%%% PREGUNTA C
+
+%%% PREGUNTA D
