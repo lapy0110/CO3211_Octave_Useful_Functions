85286b4f8e333ccb8c35238681546b0caf53a418
Luis Pino <lapy0110@gmail.com>
2018-11-04 22:08:01 -0400

    Agregado archivo modelo para las entregas

---
+++ Model.m
@@ -0,0 +1,10 @@
+% CO3211 - Calculo Numerico
+% Trimestre: Septiembre - Diciembre 2018
+% Autor: Luis Pino (15-11138)
+
+%                                                    TITULO AQUÍ
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% EJERCICIO %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% PREGUNTA:
