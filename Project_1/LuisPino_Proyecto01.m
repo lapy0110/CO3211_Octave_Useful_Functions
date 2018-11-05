7e20ae68d8f0e2e39fd27629fcd8f1c7babf7491
Luis Pino <lapy0110@gmail.com>
2018-11-05 19:01:08 -0400

    Mejorado el proyecto

--- Project_1/LuisPino_Proyecto01.m
+++ Project_1/LuisPino_Proyecto01.m
@@ -5,29 +5,31 @@
 %                                               Proyecto 1 - 05/11/2018
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PROCEDIMIENTO 1 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Creación y carga de matrices utiles durante el proceso del proyecto 
 
-% Como primera idea practica, genere vectores y!=0 en forma aleatoria, resuelva Az = y y consiga
-% la mejor cota inferior posible de la norma de A↑-1, y por ende de cond(A). Compare con la cond(A) exacta
-% que ofrece MATLAB en norma 1 y norma infinito. Por favor, tome ventaja de la factorizacion LU
-% de A ya obtenida al resolver el sistema original, en esto radica la eficiencia.
+load('CO3211_proy1datos.mat')
+A6=[0.641 0.242; 0.321 0.121];
+A7=[10 -7 0; -3 2 6; 5 -1 5];
 
-A1=[0.641 0.242; 0.321 0.121];
-A2=[10 -7 0; -3 2 6; 5 -1 5];
-A3=
-A4=
-A5=
-A6=
-A7=
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PROCEDIMIENTO 1 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+display('NUMERO DE CONDICIÓN A TRAVES DEL PROCEDIMIENTO 1')
+% Para A1
 
-Y1= rand(length(A1),1);
-Y2= rand(length(A2),1);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PROCEDIMIENTO 2 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+display('')
+display('Para la Matriz A1')
+
+CondAproxInf1=CondInf(A1)
+CondAprox11=Cond1(A1)
+CondExactoInf1=cond(A1,inf)
+CondExacto11=cond(A1,1)
+
+
+%%% PROCEDIMIENTO 2 
 
 %Luego, como segunda idea, concentre su atencíon en vectores y cuyas componentes sean 1 o −1
 %escogidos en forma aleatoria de nuevo, y obtenga cotas inferiores para la cond(A).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PROCEDIMIENTO 3 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% PROCEDIMIENTO 3 
 
 % Por  ́ultimo considere la siguiente idea: resolver primero el sistema Ax = b donde bi = 1/n para
 % todo i; luego resolver A↑t*z=y donde yi = 1 si xi ≥ 0, y yi = −1 si xi < 0; asignar al  ́ındice
